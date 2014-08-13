67d517de1d2286cd7d656286d9daca41c3e1deb1
Justin Cano <jcano001@ucr.edu>
2014-08-12 22:18:46 -0700

    redirect successful sign-up to log-in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		redirect_to articles_path, :notice => "Signed up!"
+  		redirect_to new_session_path, :notice => "Signed up!"
   	else
   		render :action => "new", :layout => false
   	end
