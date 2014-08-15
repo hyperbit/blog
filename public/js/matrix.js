845164a0b4f87f115c57c3bc03c50015e99a6be7
Justin Cano <jcano001@ucr.edu>
2014-08-14 17:31:33 -0700

    added splash page

---
+++ public/js/matrix.js
@@ -0,0 +1,58 @@
+/* Matrix courtesy of thecodeplayer.com */
+var c = document.getElementById("matrix");
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+var ctx, canvas = 0x00;
+if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+     //Do Firefox-related activities
+     canvas = document.createElement("canvas");
+     canvas.setAttribute("width",c.width);
+     canvas.setAttribute("height",c.height);
+     ctx = canvas.getContext('2d');
+}
+else {
+	ctx = document.getCSSCanvasContext("2d", "matrix", c.width, c.height);
+}
+
+//chinese characters - taken from the unicode charset
+var chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
+//converting the string into an array of single characters
+chinese = chinese.split("");
+
+var font_size = 12;
+var columns = c.width/font_size; //number of columns for the rain
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for(var x = 0; x < columns; x++)
+	drops[x] = 1; 
+
+function draw() {
+	console.log("drawing");
+	//Black BG for the canvas
+	//translucent BG to show trail
+	ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+	ctx.fillRect(0, 0, c.width, c.height);
+
+	ctx.fillStyle = "#0F0"; //green text
+	ctx.font = font_size + "px arial";
+	//looping over drops
+	for(var i = 0; i < drops.length; i++)
+	{
+		//a random chinese character to print
+		var text = chinese[Math.floor(Math.random()*chinese.length)];
+		//x = i*font_size, y = value of drops[i]*font_size
+		ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+		//sending the drop back to the top randomly after it has crossed the screen
+		//adding a randomness to the reset to make the drops scattered on the Y axis
+		if(drops[i]*font_size > c.height && Math.random() > 0.975)
+			drops[i] = 0;
+
+		//incrementing Y coordinate
+		drops[i]++;
+	}
+	document.mozSetImageElement("matrix",canvas)
+}
+setInterval(draw,33);
